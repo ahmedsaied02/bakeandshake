738b37b85ec4f2373258f43aca9d359b4f7a4ac3
ahmed saied <ahmed.saiedd02@gmail.com>
2024-06-13 13:11:30 +0300

    menu done

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,12 @@
 import type { MetaFunction } from "@remix-run/node";
-
+import page1 from "~/assets/menu1.png";
+import page2 from "~/assets/menu2.png";
+import page3 from "~/assets/menu3.png";
+import page4 from "~/assets/menu4.png";
+import page5 from "~/assets/menu5.png";
+import page6 from "~/assets/menu6.png";
+import page7 from "~/assets/menu7.png";
+import page8 from "~/assets/menu8.png";
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -9,33 +16,15 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="bg-black">Hi anoos</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex flex-col gap-2">
+      <img src={page1} alt="page1" />
+      <img src={page2} alt="page2" />
+      <img src={page3} alt="page3" />
+      <img src={page4} alt="page4" />
+      <img src={page5} alt="page5" />
+      <img src={page6} alt="page6" />
+      <img src={page7} alt="page7" />
+      <img src={page8} alt="page8" />
     </div>
   );
 }
