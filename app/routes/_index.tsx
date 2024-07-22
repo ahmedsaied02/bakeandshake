ebba6696574b9213642e5691dca0c318339ed4a7
ahmed saied <ahmed.saiedd02@gmail.com>
2024-07-22 22:43:50 +0300

    test new design

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,21 +12,21 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="h-screen w-full flex flex-col  bg-primary items-center w-full">
+    <div className="h-screen w-full flex flex-col  bg-primary items-center overflow-hidden ">
       <img src={logo} alt="Bake and Shake Logo" className="w-[50%] my-10"/>
       <Link to="/cafe">
       <div className="w-[90%] flex flex-row  m-auto align-center h-[150px] relative rounded-lg overflow-hidden">
         <img src={cafe} alt="" className="w-full  object-cover blur-[4px]" />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <span className="text-xl text-white bg-black bg-opacity-15 py-4 px-16 rounded-md">Coffee & Shakes</span>
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-20">
+          <span className="text-xl text-white  ">Coffee & Shakes</span>
         </div>
       </div>
       </Link>
       <Link to="/bakery">
       <div className="w-[90%] flex flex-row  m-auto align-center h-[150px] relative rounded-lg overflow-hidden mt-5">
         <img src={bakery} alt="" className="w-full  object-cover blur-[4px]" />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <span className="text-xl text-white bg-black bg-opacity-15 py-4 px-16 rounded-md">Bakery</span>
+        <div className="absolute inset-0 flex items-center justify-center  bg-black bg-opacity-20">
+          <span className="text-xl text-white ">Bakery</span>
         </div>
       </div>
       </Link>
