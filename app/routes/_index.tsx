6801130f121d71372bc35bcf5d1ef18fb28ed90d
ahmed saied <ahmed.saiedd02@gmail.com>
2024-07-23 05:06:21 +0300

    android problems

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,7 +16,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="h-screen w-full flex flex-col  bg-primary items-center overflow-hidden ">
+    <div className=" w-full flex flex-col  bg-primary items-center overflow-hidden ">
       <Header />
       <Link to="/cafe">
       <div className="mt-5 w-[90%] flex flex-row  m-auto align-center h-[150px] relative rounded-lg overflow-hidden">
@@ -28,7 +28,7 @@ export default function Index() {
       </Link>
       <Link to="/bakery">
       <div className="w-[90%] flex flex-row  m-auto align-center h-[150px] relative rounded-lg overflow-hidden mt-5">
-        <img src={bakery} alt="" className="w-full  object-cover blur-[4px]" />
+        <img src={bakery} alt="" className="w-[100%]  object-cover blur-[4px]" />
         <div className="absolute inset-0 flex items-center justify-center  bg-black bg-opacity-20">
           <span className="text-xl text-white ">Bakery</span>
         </div>
