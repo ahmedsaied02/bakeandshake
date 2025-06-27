107dabd086a26bd390c4876c523cf2d3d747e01d
ahmed saied <ahmed.saied02@gmail.com>
2025-06-27 18:13:50 +0300

    fixes

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
   return (
   	<div style={{ height: "100vh" }}>
       <iframe
-        src="../../public/assets/menu.pdf"
+        src="/assets/menu.pdf"
         width="100%"
         height="100%"
         title="PDF Viewer"
